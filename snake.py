873d1e5041b59cc04b7569625e25cb15d02e6cab
Prikaziuk <prikaziuk@gmail.com>
2015-03-12 19:30:19 +0300

    Corrected snake

--- snake.py
+++ snake.py
@@ -3,60 +3,55 @@ __author__ = 'Admin'
 
 import random
 
+DOWN = 's'
+UP = 'w'
+RIGHT = 'd'
+LEFT = 'a'
 
-class Canvas:
-    def __init__(self, width, height, fill='.'):
+class Field:
+    def __init__(self, width, height):
         self._width = width
         self._height = height
-        self._fill = fill
-        self._screen = [[fill] * width for _ in range(height)]
-        self._figures = []
+        self._screen = [['_'] * width for _ in range(height)]
 
 
-    def put_snake(self, x, y, fill):
+    def put_snake(self, x, y):
         if x < 0 or x >= self._width or y < 0 or y >= self._height:
             return
         self._x = x
         self._y = y
-        self._screen[y][x] = fill
+        self._screen[y][x] = 'x'
 
     def move_snake(self, direction):
-        if direction == 'w':
+        self._screen[self._y][self._x] = '_'
+        if direction == UP:
             self._y -=1
-        elif direction == 's':
+        elif direction == DOWN:
             self._y +=1
-        elif direction == 'a':
+        elif direction == LEFT:
             self._x -=1
-        elif direction == 'd':
+        elif direction == RIGHT:
             self._x +=1
+        self._screen[self._y][self._x] = 'x'
 
-
-    def put_apple(self, number, fill):
-        while number:
-            self._x = random.randint(0, self._width)
-            self._y = random.randint(0, self._height)
-            self._screen[self._x][self._y] = fill
-            number -=1
+    def put_apple(self, number):
+        for _ in range(number):
+            self._screen[random.randint(0, self._width-1)][random.randint(0, self._height-1)] = 'o'
 
     def paint(self):
-        for figure in self._figures:
-            figure.paint()
         for line in self._screen:
             print(''.join(line))
         print()
 
-
-
-
 def main():
-    canvas = Canvas(10, 10)
-    canvas.paint()
-    canvas.put_apple(3, 'o')
-    canvas.paint()
-    canvas.put_snake(0, 0, 'x')
-    canvas.paint()
-    canvas.move_snake('s')
-    canvas.paint()
+    field = Field(10, 10)
+    field.paint()
+    field.put_apple(5)
+    field.paint()
+    field.put_snake(0, 0)
+    field.paint()
+    field.move_snake('s')
+    field.paint()
 
 if __name__ == '__main__':
     main()
