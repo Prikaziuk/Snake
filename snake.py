85add4fb51ed7d2a1ac18a34c052ef18933aee6f
Prikaziuk <prikaziuk@gmail.com>
2015-05-06 17:55:18 +0300

    First way of snake_move correction

--- snake.py
+++ snake.py
@@ -1,7 +1,11 @@
 __author__ = 'Admin'
 #!/usr/bin/env python3
 
+# run like ./snake.py number_of_apples
+
 import random
+#import sys sys.argv[1] = number_of_apples
+
 
 # some constants that will be used
 
@@ -16,8 +20,9 @@ SNAKE = 'z'
 snake_length = 1
 snake_trace = []
 
-# sign of apple
+# apple parmeters
 APPLE = 'o'
+NUMBER_OF_APPLES = 5
 
 class Field:
 # create a field
@@ -33,6 +38,7 @@ class Field:
         self._screen[x][y] = SNAKE
         snake_trace.append([self._x, self._y])
 
+# moves snake in the given direction
     def move_snake(self, direction, snake_length = snake_length):
         # remove snake from the screen
         for coord in snake_trace[-snake_length:]:
@@ -47,16 +53,22 @@ class Field:
             self._x +=1
         # append new coordinates of snake
         snake_trace.append([self._x, self._y])
-        if self._screen[self._y][self._x] == APPLE:
-            snake_length +=1
+        try:
+            if self._screen[self._y][self._x] == APPLE:
+                snake_length +=1
+        except IndexError:
+            print("Game over")
+            quit()
         # add whole snake to the screen again
         for coord in snake_trace[-snake_length:]:
             self._screen[coord[1]][coord[0]] = SNAKE
 
-    def put_apples(self, number):
+# randomly put "number" quantity of apples on the screen
+    def put_apples(self, number = NUMBER_OF_APPLES):
         for _ in range(number):
             self._screen[random.randint(0, self._width-1)][random.randint(0, self._height-1)] = APPLE
 
+# paint all what is on the field
     def paint(self):
         for line in self._screen:
             print(''.join(line))
@@ -64,13 +76,15 @@ class Field:
 
 def main():
     field = Field(10, 10)
-    field.paint()
-    field.put_apples(5)
-    field.paint()
+    field.put_apples()
     field.put_snake()
     field.paint()
-    field.move_snake('s')
-    field.paint()
+    while True:
+        field.move_snake(input())
+        field.paint()
+        if snake_length == NUMBER_OF_APPLES + 1:
+            print("Finish")
+            break
 
 
 if __name__ == '__main__':
