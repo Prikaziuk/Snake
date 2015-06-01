f27435e18ba4fa6b5b5fb54a319c26aee78c5139
Prikaziuk <prikaziuk@gmail.com>
2015-06-01 19:51:10 +0300

    tkinter_working
    Incorrect docstrings and confusing function names

--- tk_snake.py
+++ tk_snake.py
@@ -6,131 +6,26 @@ Python version of the common game "Snake and apples".
 Usage: snake.py [number_of_apples]
 """
 from random import randrange
-from collections import deque
-import tkinter as tk
+from snake import Snake, Field
+from tkinter import *
+from tkinter import messagebox
 
+root = Tk()
 # import sys sys.argv[1] = number_of_apples
 
 """
 Some constants that will be used: snake direction (str), symbols of snake (str) and apple (str)
  and number of apples to appear on the field (int)
 """
-DOWN = 's'
-UP = 'w'
-RIGHT = 'd'
-LEFT = 'a'
+DOWN = "DOWN"
+UP = 'UP'
+RIGHT = "RIGHT"
+LEFT = 'LEFT'
 
 SNAKE = 'z'
 APPLE = 'o'
 
 NUMBER_OF_APPLES = 5
-
-
-class Field:
-    """
-    Information about the field.
-
-    Attributes:
-        width (int) : width of the field
-        height (int) : height of the field
-    """
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
-
-    def width(self):
-        """
-        :return: (int)
-            width
-        """
-        return self._width
-
-    def height(self):
-        """
-        :return: (int)
-            height
-        """
-        return self._height
-
-
-class Snake:
-    """
-    Information about the snake.
-    """
-    def __init__(self, head_coord):
-        """
-        Arg:
-            head_coord (tuple) : coordinates of the snake head (y, x)
-        """
-        self._snake_coord = deque([head_coord])
-
-    def __len__(self):
-        """
-        :return:
-            int : length of the snake
-        """
-        return len(self._snake_coord)
-
-    def __iter__(self):
-        """
-        :return: tuple from the list of tuples
-        """
-        return iter(self._snake_coord)
-
-    def head(self):
-        """
-        :return:
-            (tuple) : coordinates of the snake head (y, x)
-        """
-        return self._snake_coord[-1]
-
-    def snake_coordinates(self):
-        """
-        :return:
-            (list of tuples) : list of coordinates of the snake (y, x)
-        """
-        return self._snake_coord
-
-    def move(self, position, is_apple):
-        """
-        Moves the snake in the given direction
-
-        :param position (tuple) : new coordinates of the snake head (new step direction)
-        :param is_apple (bool) : True if apple is at the position, False otherwise
-        :return: None
-        """
-        self._snake_coord.append(position)
-        if not is_apple:
-            self._snake_coord.popleft()
-
-    def revert(self):
-        """
-        Reverts the snake
-
-        :return: None
-        """
-        self._snake_coord.reverse()
-
-
-def user_input():
-    """
-    When everything is well
-    :return:
-        direction (str) : DOWN, UP, RIGHT, LEFT
-
-    When something goes wrong (typo in input)
-    :print:
-        (str) : "input() does not give any direction. Available directions are: DOWN, UP, RIGHT,LEFT"
-                 "Please, type valid command to set snake direction:"
-        and asks user for new input of direction
-    """
-    direction = input()
-    while direction not in [DOWN, UP, RIGHT, LEFT]:
-        print("'{}' does not set any direction. \n Available directions are:"
-              " \n \tDOWN : '{}' \n \tUP : '{}' \n \tRIGHT : '{}' \n \tLEFT : '{}' \n "
-              "Please, type valid command to set snake direction:".format(direction, DOWN, UP, RIGHT, LEFT))
-        direction = input()
-    return direction
 
 
 class Game:
@@ -140,12 +35,10 @@ class Game:
     Attributes:
         field (class) : class with parameters of the field for a game
         snake (class) : class with parameters of snake
-        user (function) : user_input()
     """
-    def __init__(self, field, snake, user):
+    def __init__(self, field, snake):
         self._field = field
         self._snake = snake
-        self._user_function = user
         self._apples_positions = set()
         self._direction_action = {
             LEFT: lambda x, y: (y, (x - 1) % self._field.width()),
@@ -153,6 +46,8 @@ class Game:
             UP: lambda x, y: ((y - 1) % self._field.height(), x),
             DOWN: lambda x, y: ((y + 1) % self._field.height(), x)
         }
+        self._canvas = Canvas(root, width=self._field.width()*10, height=self._field.height()*10, bg='green')
+        self._canvas.pack()
 
     def put_apples(self, number=NUMBER_OF_APPLES):
         """
@@ -164,12 +59,11 @@ class Game:
         while len(self._apples_positions) < number and self._snake.head() not in self._apples_positions:
             self._apples_positions.add((randrange(self._field.height()), randrange(self._field.width())))
 
-    def _move_snake(self):
+    def _move_snake(self, direction):
         """
         Works with user input: transforms direction into snake_head coordinates (position)
         :return: None
         """
-        direction = self._user_function()
         y, x = self._snake.head()
         position = self._direction_action[direction](x, y)
         is_apple = position in self._apples_positions
@@ -179,24 +73,42 @@ class Game:
             self._snake.revert()
         else:
             self._snake.move(position, is_apple)
+        self.paint_snake()
+        self.paint_apples()
+
+    def paint_snake(self):
+        self.check_snake()
+        self._canvas.delete('snake')
+        for coord in self._snake:
+            self._canvas.create_rectangle(coord[1]*10, coord[0]*10, coord[1]*10 + 10, coord[0]*10 + 10,
+                                          outline='white', fill='blue', tag='snake')
+        self.win_check()
+
+    def paint_apples(self):
+        self._canvas.delete('apple')
+        for coord in self._apples_positions:
+            self._canvas.create_oval(coord[1]*10, coord[0]*10, coord[1]*10+10, coord[0]*10+10,
+                                     fill='red', tag='apple')
 
     def paint(self):
         """
-        Prints field with apples and snake at the terminal
+        Prints field with apples and snake and creates buttons
         :return: None
         """
-        root = tk.Tk()
-        canvas = tk.Canvas(root, width = self._field.width()*10+10, height = self._field.height()*10+10)
-        screen = [['_'] * self._field.width() for _ in range(self._field.height())]
-        for coord in self._apples_positions:
-            screen[coord[0]][coord[1]] = APPLE
-            canvas.create_rectangle(coord[1]*10-5, coord[0]*10-5, coord[1]*10+5, coord[0]*10+5, fill = 'red')
-        for coord in self._snake:
-            screen[coord[0]][coord[1]] = SNAKE
-            canvas.create_rectangle(coord[1]*10-5, coord[0]*10-5, coord[1]*10+5, coord[0]*10+5, fill = 'green')
-        for line in screen:
-            print(''.join(line))
-        canvas.pack()
+        but = Button(root, text='LEFT')
+        but.pack(side="left")
+        but.bind("<Button-1>", lambda event: self._move_snake('LEFT'))
+        but2 = Button(root, text='RIGHT')
+        but2.pack(side='right')
+        but2.bind("<Button-1>", lambda event: self._move_snake('RIGHT'))
+        but3 = Button(root, text='UP')
+        but3.pack(side='top')
+        but3.bind("<Button-1>", lambda event: self._move_snake('UP'))
+        but4 = Button(root, text='DOWN')
+        but4.pack(side='bottom')
+        but4.bind("<Button-1>", lambda event: self._move_snake('DOWN'))
+        self.paint_snake()
+        self.paint_apples()
         root.mainloop()
 
     def check_snake(self):
@@ -207,7 +119,7 @@ class Game:
         :print: "GAME OVER! Your snake crashed" if snake moves into itself and interrupts programme
         """
         if len(self._snake) != len(set(self._snake.snake_coordinates())):
-            print("\n GAME OVER! Your snake crashed")
+            messagebox.showinfo("GAME OVER!", "Your snake crashed")
             quit()
 
     def start(self):
@@ -220,11 +132,11 @@ class Game:
         """
         self.put_apples()
         self.paint()
-        while len(self._snake) < NUMBER_OF_APPLES + 1:
-            self._move_snake()
-            self.check_snake()
-            self.paint()
-        print("\n YOU WIN! All apples are safely collected")
+
+    def win_check(self):
+        if len(self._apples_positions) == 0:
+            messagebox.showinfo("YOU WIN!", "All apples are safely collected")
+            quit()
 
 
 def main():
@@ -234,9 +146,8 @@ def main():
     """
     field = Field(10, 10)
     snake = Snake((0, 0))
-    game = Game(field, snake, user_input)
+    game = Game(field, snake)
     game.start()
-
 
 if __name__ == '__main__':
     main()
